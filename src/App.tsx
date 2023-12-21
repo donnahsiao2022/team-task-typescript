0cd4908726c29f32a6f2356d98736d82c1f9856e
CHAN-CHENG <ccc908925@gmail.com>
2023-12-21 11:11:25 +0800

    feat: Routes 加入 NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages';
+import { Home, NotFound } from './pages';
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-      </Routes>
+      <div className="bg-dark">
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
+        </Routes>
+      </div>
     </>
   );
 };
