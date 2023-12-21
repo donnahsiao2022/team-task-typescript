a7e552801234637c7f05d843742f7775decdd929
CHAN-CHENG <ccc908925@gmail.com>
2023-12-21 16:17:29 +0800

    feat: build 出的路徑調整

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 // https://dev.to/boostup/uncaught-referenceerror-process-is-not-defined-12kg
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
       'process.env': env,
     },
     plugins: [react()],
-  }
-})
+    base: '/team-task-typescript/',
+  };
+});
